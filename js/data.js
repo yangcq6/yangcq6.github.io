5063c6bf068892f6e4cd59601c722fa9a4aee399
yangcq <kyangcq@163.com>
2024-03-05 11:21:05 +0800

    test vue cart

---
+++ js/data.js
@@ -0,0 +1,38 @@
+const books = [
+  {
+    id: 1,
+    name: '兰博基尼',
+    date: '2024-1',
+    price: 2.00,
+    count: 1
+  },
+  {
+    id: 2,
+    name: '梵克雅宝10斤大项链',
+    date: '2022-12',
+    price: 3.00,
+    count: 1
+  },
+  {
+    id: 3,
+    name: 'Iphone 50 Pro Max Ultra Meta Mix',
+    date: '2099-10',
+    price: 2.00,
+    count: 1
+  },
+  {
+    id: 4,
+    name: '保时捷全型号车100俩',
+    date: '2022-12',
+    price: 5.00,
+    count: 1
+  },
+  {
+    id: 5,
+    name: '10亿美金',
+    date: '2022-12',
+    price: 3.00,
+    count: 1
+  },
+]
+
